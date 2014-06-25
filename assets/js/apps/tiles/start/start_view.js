be3a63575b2be607509c64b9d5f6a88cfa8317ab
hanishi <h.nishi@mosa-architect.com>
2014-06-26 02:03:21 +0900

    updated

--- assets/js/apps/tiles/start/start_view.js
+++ assets/js/apps/tiles/start/start_view.js
@@ -1,7 +1,18 @@
 TilesManager.module("TilesApp.Start", function(Start, TilesManager, Backbone, Marionette, $, _) {
+    var template = '<div class="square bg <%- color %>">' +
+        '<div class="content">' +
+        '<div class="table">'+
+            '<div class="table-cell"></div>'+
+        '</div>'+
+        '</div>'+
+    '</div>';
     Start.Tile = Marionette.ItemView.extend({
-        el: "",
-        template: "#tiles-tile",
+
+        template: function(serialized_model) {
+            var color = serialized_model.color;
+            return _.template(template, {color: color})
+        },
+
         events: {
             "click": "tileClicked"
         },
@@ -10,7 +21,17 @@ TilesManager.module("TilesApp.Start", function(Start, TilesManager, Backbone, Ma
             e.stopPropagation();
 
             this.trigger("tiles:switch", this.model);
+        },
+        onRender: function () {
+            // Get rid of that pesky wrapping-div.
+            // Assumes 1 child element present in template.
+            this.$el = this.$el.children();
+            // Unwrap the element to prevent infinitely
+            // nesting elements during re-render.
+            this.$el.unwrap();
+            this.setElement(this.$el);
         }
+
     });
 
     Start.Tiles = Marionette.CollectionView.extend({
